ad648c6d7e99a08014de0488fe56819084e2a3a0
Osvaldo Pacheco <osvaldont@gmail.com>
2019-05-14 18:52:48 -0300

    Tela de Login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,17 +9,20 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $defauts = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
 
 	public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/"){
 
+				
 		$this->options = array_merge($this->defauts, $opts);
 
 		// config
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. "/ecommerce/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]. "/ecommerce/views-cache/",
 			"debug"         => false
 		   );
@@ -30,7 +33,7 @@ class Page{
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -50,7 +53,7 @@ class Page{
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -4,8 +4,6 @@ namespace Hcode;
 
 class PageAdmin extends Page{
 
-	echo "Aqui";
-
 	public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/admin/"){
 
 		parent::__construct($opts, $tpl_dir);
